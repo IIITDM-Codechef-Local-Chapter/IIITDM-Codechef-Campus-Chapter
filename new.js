2804219cf07e1ee21abbe32ff0947bf40b1be50f
John Zakkam <johnzakkam2592@gmail.com>
2020-03-20 17:42:35 +0530

    Create new.js

---
+++ new.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Test</h2>
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML = 'Codechef Local Chappter';
+</script>
+
+</body>
+</html>
